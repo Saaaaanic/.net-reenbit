d53d6c97c4ac31c01373400387385cbeac4c9292
Saaaaanic <you@example.com>
2023-03-05 22:09:31 +0400

    Remove: Clicker, Weather pages. Add input lines with class User

---
+++ ReenbitTest/Data/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace ReenbitTest.Data;
+
+public class User
+{
+    [EmailAddress]
+    public string? Email { get; set; }
+    public IBrowserFile File { get; set; }
+}
