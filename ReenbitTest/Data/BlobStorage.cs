af41481f7b83d8b77b77c53b9757cc8e2db5ff39
Saaaaanic <109736471+Saaaaanic@users.noreply.github.com>
2023-03-08 14:32:17 +0400

    Update BlobStorage.cs

--- ReenbitTest/Data/BlobStorage.cs
+++ ReenbitTest/Data/BlobStorage.cs
@@ -5,7 +5,7 @@ namespace ReenbitTest;
 public class BlobStorage
 {
     private string connectionString =
-        "DefaultEndpointsProtocol=https;AccountName=netreenbit;AccountKey=WVk/a6VJhzpjsjvwoffvVtpyOcgrFm2ZQ+sO8Fa/NWN7D8rXar2WdeZBCKQDW4vh2oagn1fCwiem+AStMyPcyQ==;EndpointSuffix=core.windows.net";
+        *connection string from Azure*;
     private string containerName = "files";
     public BlobContainerClient container;
     
