8469e185539d2511b51aae559df80d98cad97901
Saaaaanic <you@example.com>
2023-03-08 14:29:31 +0400

    Add BlobStorage, uploading files to blob

---
+++ ReenbitTest/Data/BlobStorage.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Storage.Blobs;
+
+namespace ReenbitTest;
+
+public class BlobStorage
+{
+    private string connectionString =
+        "DefaultEndpointsProtocol=https;AccountName=netreenbit;AccountKey=WVk/a6VJhzpjsjvwoffvVtpyOcgrFm2ZQ+sO8Fa/NWN7D8rXar2WdeZBCKQDW4vh2oagn1fCwiem+AStMyPcyQ==;EndpointSuffix=core.windows.net";
+    private string containerName = "files";
+    public BlobContainerClient container;
+    
+    public BlobStorage()
+    {
+        container = new BlobContainerClient(connectionString, containerName);
+    }
+}
